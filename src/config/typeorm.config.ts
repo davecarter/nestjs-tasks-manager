98aa8e15bee91a0c23236a9aa0a8f68e729db14c
David G <dave74@gmail.com>
2020-11-04 18:42:46 +0100

    add entity and repository classes

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
@@ -6,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: 'postgres',
   database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: true
 }
