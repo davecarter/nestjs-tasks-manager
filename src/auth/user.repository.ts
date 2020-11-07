a60f7cf9172819c8b2cadd4b4699e8ad46c68884
David G <dave74@gmail.com>
2020-11-07 17:27:27 +0100

    add error handling and username hashing

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,31 @@
 import { Repository, EntityRepository } from 'typeorm'
 import { User } from './user.entity'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+import { ConflictException, InternalServerErrorException } from '@nestjs/common'
+import { genSalt, hash } from 'bcrypt'
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto
-    const user = new User()
-    user.username = username
-    user.password = password
 
-    await user.save()
+    const user = new User()
+    user.salt = await genSalt()
+    user.username = username
+    user.password = await this.hashPassword(password, user.salt)
+
+    try {
+      await user.save()
+    } catch (error) {
+      // duplicated username error code
+      if (error.code === '23505') {
+        throw new ConflictException('User name already exists')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
+  }
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return hash(password, salt)
   }
 }
